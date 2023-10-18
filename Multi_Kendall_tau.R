07597208bcb9151a0e3c66db7c64d81ea43da00d
Chunyin Lei <138145039+ChunyinLei@users.noreply.github.com>
2023-10-18 22:00:45 +0800

    Multi_Kendall_tau.R

--- Multi_Kendall_tau.R
+++ Multi_Kendall_tau.R
@@ -1,14 +1,22 @@
 ## Multivariate Kendall’s tau
-Multi_Kendall_tau <- function(x){
-  if(!is.matrix(x) && !is.data.frame(x)){
-    stop("x must be either numeric vector, matrix or data.frame.")}
-  n <- nrow(x)
+Multi_Kendall_tau <- function(X, Y){
+  if(!is.matrix(X) && !is.data.frame(X)){
+    stop("X must be either numeric vector, matrix or data.frame.")}
+  if(!is.matrix(Y) && !is.data.frame(Y)){
+    stop("Y must be either numeric vector, matrix or data.frame.")}
+  
+  n <- nrow(X)
   s <- 0
-  for (j in 1:(n - 1)){
-    for (i in (j + 1):n){
-      s <- s + ((x[i, ] - x[j, ]) %*% t(x[i, ] - x[j, ])) / (norm((x[i, ] - x[j, ]), '2') ^ 2)
-      ret <- s / choose(n, 2)
+  for(j in 1:(n - 1)){
+    for(i in (j + 1):n){
+      if((Y[i, ] - Y[j, ]) == 0){
+        s <- s + 0
+      }else{
+        s <- s + (as.matrix(X[i, ] - X[j, ]) %*% t(Y[i, ] - Y[j, ])) / (norm((X[i, ] - X[j, ]), '2') * norm((Y[i, ] - Y[j, ]), '2'))
+      }
     }
   }
+  ret <- s / choose(n, 2)
+  
   return(ret)
 }
